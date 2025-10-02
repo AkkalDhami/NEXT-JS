4f0cac0085201ca75bb8493ad5fcf3fe95c146e3
AkkalDhami <dhamiakkal21@gmail.com>
2025-10-02 08:57:02 +0545

    Handling DELETE Request in Next.js

--- todo-app/src/app/todos/[id]/route.js
+++ todo-app/src/app/todos/[id]/route.js
@@ -57,3 +57,32 @@ export async function PATCH(req, { params }) {
     }
   );
 }
+
+export async function DELETE(req, { params }) {
+  const { id } = await params;
+  console.log(id);
+  const todo = todos.find((todo) => todo.id !== id);
+  console.log(todo);
+  if (!todo) {
+    return Response.json(
+      {
+        message: "Todo Not Found",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  const newTodos = todos.filter((todo) => todo.id !== id);
+  await writeFile("todos.json", JSON.stringify(newTodos, null, 4));
+  return Response.json(
+    {
+      message: "Todo deleted successfully",
+      todo,
+    },
+    {
+      status: 200,
+    }
+  );
+}
